9bbc8aaae5a102095cf1afa73773d29f6c256e7a
Jakub Malyjasiak <malyjasiak@gmail.com>
2018-03-03 00:59:05 +0100

    B23 Create app and security module basics.

---
+++ server/security/src/test/java/com/streamacho/api/SecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.streamacho.api;
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class SecurityApplicationTests {
+
+     @Test
+     public void contextLoads() {
+     }
+}
