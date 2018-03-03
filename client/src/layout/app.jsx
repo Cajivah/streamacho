ea04675ad3b3f914de91bfbedc5c7cbc3d88c690
Wojciech Bator <wojciech.bator@sienn.pl>
2018-03-03 15:05:52 +0100

    Basic store configured

--- client/src/layout/app.jsx
+++ client/src/layout/app.jsx
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
-import LandingPage from './landing/landing';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import LandingPage from './landing/landing';
 import Fallback from './fallback/fallback';
 import Register from './register/register';
 import Login from './login/login';
+import { store } from '../store/store';
 import './app.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Router>
-          <div>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-            <Route component={Fallback} />
-          </div>
-        </Router>
-      </div>
+      <Provider store={store}>
+        <div className="app">
+          <Router>
+            <div>
+              <Route exact path="/" component={LandingPage} />
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+              <Route component={Fallback} />
+            </div>
+          </Router>
+        </div>
+      </Provider>
     );
   }
 }
