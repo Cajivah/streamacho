6b78927f956206a8e6b0f9cb0e3061493add9168
Wojciech Bator <wojciech.bator@sienn.pl>
2018-03-03 11:34:09 +0100

    react-redux

--- client/src/layout/app.jsx
+++ client/src/layout/app.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router';
 import LandingPage from './landing/landing';
-import Navigation from './navigation/navigation';
+import Fallback from './fallback/fallback';
+import Register from './register/register';
+import Login from './login/login';
 import './app.css';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Navigation />
-        <LandingPage />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route component={Fallback} />
+        </Switch>
       </div>
     );
   }
--- client/src/layout/landing/landing.jsx
+++ client/src/layout/landing/landing.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Navigation from '../navigation/navigation';
 import './landingPage.css';
 
 export default class LandingPage extends Component {
   render() {
     return (
-      <div className='landing-page'>
-        Landing Page here
+      <div>
+        <Navigation />
+        <div className='landing-page'>
+          Landing Page here
+      </div>
       </div>
     );
   }
