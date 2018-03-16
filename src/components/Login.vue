14b49f069251d67c09066ac55c8daa0a85ebaf91
WojtekBator <wojciech.bator@hubplanner.com>
2018-03-16 15:56:48 +0100

    register and login, no posts to backend Yet

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,94 @@
 <template>
-    <div>Login page</div>
+  <form class="content login" @submit.prevent="onSubmit()">
+    <div class="login-container">
+        <h2 class="is-large">Please, login</h2>
+        <div class="field">
+        <label class="label">Email</label>
+        <div class="control has-icons-left">
+            <input
+            class="input"
+            name="email"
+            placeholder="email"
+            v-model="loginForm.email"
+            v-validate="'required|email'"
+            > 
+            <span class="icon is-small is-left">
+            <i class="fa fa-envelope"></i>
+            </span>
+        </div>
+        <p class="help is-danger">{{ errors.first('email') }}</p>
+        </div>
+        <div class="field">
+        <label class="label">Password</label>
+        <div class="control has-icons-left">
+            <input
+            class="input"
+            name="password"
+            placeholder="password"
+            type="password"
+            v-model="loginForm.password"
+            v-validate="'required|min:5'"
+            >
+            <span class="icon is-small is-left">
+            <i class="fa fa-key"></i>
+            </span>
+        </div>
+        <p class="help is-danger">{{ errors.first('password') }}</p>
+        </div>
+        <div class="field">
+            <button class="button is-primary">Login</button>
+        </div>
+        <router-link to='register'>Need to register? Click me</router-link>
+    </div>
+  </form>
 </template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      loginForm: {
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$validator.validateAll().then(result => {
+        if (!result) {
+          return;
+        }
+        this.$emit("onLogin", {
+          ...this.loginForm
+        });
+        this.loginForm.email = "";
+        this.loginForm.password = "";
+        this.$validator.reset();
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+  background-image: url("../assets/login.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.login-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 40px;
+  background-color: rgba(240, 240, 240, 0.6);
+}
+</style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,11 +1,15 @@
 <template>
-<nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <router-link class="navbar-item" to="register">Register</router-link>
-        <div class="navbar-burger">
-            <span></span>
-            <span></span>
-            <span></span>
+<nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+    <div class="navbar-shadow"></div>
+    <div class="container">
+        <div class="navbar-brand">
+            <router-link class="navbar-item" to="register">Register</router-link>
+            <router-link class="navbar-item" to="login">Login</router-link>
+            <div class="navbar-burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </div>
     </div>
 </nav>
