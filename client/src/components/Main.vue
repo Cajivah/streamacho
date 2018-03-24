a626f63ff86a61b474a43bdd07194bdbd4c463fc
WojtekBator <wojciech.bator@hubplanner.com>
2018-03-24 21:24:59 +0100

    renamed to client - good branch ffs

---
+++ client/src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <navigation></navigation>
+    <div class="container">
+      <meetings-hub></meetings-hub>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MeetingsHub from '@/components/meetings/MeetingsHub';
+import Navigation from '@/components/Navigation';
+
+export default {
+  name: 'app',
+  components: {
+    MeetingsHub,
+    Navigation
+  }
+}
+</script>
---
+++ client/src/components/meetings/MeetingList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul>
+      <meeting v-bind:key="meetings.indexOf(meeting)" v-for="meeting in meetings" :meeting="meeting" @onRemoveMeeting="removeMeeting"></meeting>
+    </ul>
+    <p v-if="!meetings.length">No meetings!</p>
+  </div>
+</template>
+
+<script>
+import Meeting from '@/components/meetings/Meeting.vue';
+
+  export default {
+    name: "meetingList",
+    components: {
+      Meeting
+    },
+    props: {
+      meetings: {
+        type: Array,
+      }
+    },
+    methods: {
+      removeMeeting(meeting) {
+        this.$emit('onRemoveMeeting', meeting);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
