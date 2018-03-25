ee15f7f646bcd4060bf671b1ecd0a7385eefe592
WojtekBator <wojciech.bator@hubplanner.com>
2018-03-25 14:01:51 +0200

    proper google auth

--- client/src/config.js
+++ client/src/config.js
@@ -2,12 +2,12 @@ export const authConfig = {
     baseUrl: 'http://localhost:3000',
     providers: {
         google: {
-            clientId: 'streamacho-199110',
-            redirectUri: 'http://localhost:8080/auth/google_callback'
+            clientId: '77624847563-iufcd6dluq2scf9nv2bb80tidp7fnvld.apps.googleusercontent.com',
+            redirectUri: 'http://localhost:8080'
         },
         facebook: {
             clientId: '214269489323711',
-            redirectUri: 'http://localhost:8080/auth/facebook_callback'
+            redirectUri: 'http://localhost:8080'
         }
     }
 }
