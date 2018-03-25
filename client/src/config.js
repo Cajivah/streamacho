3b2209e972ce4a6c253599d2eb8213a417194986
WojtekBator <wojciech.bator@hubplanner.com>
2018-03-25 12:04:31 +0200

    added facebook and google projects to authorize

--- client/src/config.js
+++ client/src/config.js
@@ -2,11 +2,11 @@ export const authConfig = {
     baseUrl: 'http://localhost:3000',
     providers: {
         google: {
-            clientId: '',
+            clientId: 'streamacho-199110',
             redirectUri: 'http://localhost:8080/auth/google_callback'
         },
         facebook: {
-            clientId: '',
+            clientId: '214269489323711',
             redirectUri: 'http://localhost:8080/auth/facebook_callback'
         }
     }
